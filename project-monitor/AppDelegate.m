cbef4d6c57dda6278227a0517eef8ab96f8aa454
Dimitri Roche <dimroc@dimroc-mb15.home>
2014-02-02 15:08:36 -0500

    Add parse.h to precompiled header

--- project-monitor/AppDelegate.m
+++ project-monitor/AppDelegate.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Dimitri Roche. All rights reserved.
 //
 
-#import <Parse/Parse.h>
 #import "ParseHelper.h"
 #import "AppDelegate.h"
 
--- project-monitor/MasterViewController.m
+++ project-monitor/MasterViewController.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Dimitri Roche. All rights reserved.
 //
 
-#import <Parse/Parse.h>
 #import "MasterViewController.h"
 
 #import "DetailViewController.h"
