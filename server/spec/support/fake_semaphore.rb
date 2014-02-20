e824951d409319d5ad79e381d5e157747e948b12
Dimitri Roche <dimroc@dimroc-mb15.home>
2014-02-19 23:27:23 -0500

    Add FakeSemaphore toggleable fake

--- server/spec/support/fake_semaphore.rb
+++ server/spec/support/fake_semaphore.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 
-class FakeParse < Sinatra::Base
-  get "/1/classes/Build", provides: :json do
+class FakeSemaphore < Sinatra::Base
+  get "/api/v1/projects/:hash_id/:id/status", provides: :json do
     erb = ERB.new(File.read(build_fixture))
     erb.result
   end
@@ -9,7 +9,7 @@ class FakeParse < Sinatra::Base
   private
 
   def build_fixture
-    File.join(File.dirname(__FILE__), "../fixtures/parse_builds.json.erb")
+    File.join(File.dirname(__FILE__), "../fixtures/semaphore_build.json.erb")
   end
 
   # start the server if ruby file executed directly
