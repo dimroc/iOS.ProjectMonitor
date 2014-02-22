2bdefb9081324662632ce64bd141b8a027bd8475
Dimitri Roche <dimroc@dimroc-mb15.home>
2014-02-22 12:47:02 -0500

    Set environment in sidekiq redis namespace

--- server/bootstrap.rb
+++ server/bootstrap.rb
@@ -1,5 +1,5 @@
-Env = ENV["RACK_ENV"] || 'development'
-Bundler.require(:default, Env)
+ENVIRONMENT = ENV["RACK_ENV"] || 'development'
+Bundler.require(:default, ENVIRONMENT)
 ActiveSupport::Dependencies.autoload_paths << File.join(File.dirname(__FILE__), "app")
 
 require_all "config"
--- server/initializers/sidekiq.rb
+++ server/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # If your client is single-threaded, we just need a single connection in our Redis connection pool
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'pm', :size => 1, :url => Settings.redis_url }
+  config.redis = { :namespace => "pm_#{ENVIRONMENT}", :size => 1, :url => Settings.redis_url }
 end
 
 # Sidekiq server is multi-threaded so our Redis connection pool size defaults to concurrency (-c)
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'pm', :url => Settings.redis_url }
+  config.redis = { :namespace => "pm_#{ENVIRONMENT}", :url => Settings.redis_url }
 end
