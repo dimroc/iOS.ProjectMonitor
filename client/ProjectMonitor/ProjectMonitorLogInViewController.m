9e1f64c2b3236916252b7404ec6cbf53b4f0386f
Dimitri Roche <dimroc@gmail.com>
2014-03-22 14:07:09 -0400

    Add app icon, update login aesthetics
    [Finishes #65900972]

--- client/ProjectMonitor/ProjectMonitorLogInViewController.m
+++ client/ProjectMonitor/ProjectMonitorLogInViewController.m
@@ -19,6 +19,24 @@
     [super viewDidLoad];
     
     [self.logInView setLogo:[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo.png"]]];
+    [self.logInView setBackgroundColor:[UIColor whiteColor]];
+    
+    [self setBorderColor:self.logInView.usernameField];
+    [self setBorderColor:self.logInView.passwordField];
+    
+    [self.logInView.usernameField setTextColor:[UIColor blackColor]];
+    [self.logInView.passwordField setTextColor:[UIColor blackColor]];
+}
+
+- (void)setBorderColor:(UITextField*)field {
+    field.layer.cornerRadius=5.0f;
+    field.layer.masksToBounds=YES;
+    field.layer.borderColor=[[UIColor darkGrayColor]CGColor];
+    field.layer.borderWidth= .2f;
+    
+    CALayer *layer = field.layer;
+    layer.shadowOpacity = 0.0;
+    layer.backgroundColor = [[UIColor lightTextColor] CGColor];
 }
 
 @end
--- client/ProjectMonitor/ProjectMonitorSignUpViewController.m
+++ client/ProjectMonitor/ProjectMonitorSignUpViewController.m
@@ -17,7 +17,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self.signUpView setLogo:[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo.png"]]];
+    [self.signUpView setBackgroundColor:[UIColor whiteColor]];
+    
+    [self setBorderColor:self.signUpView.usernameField];
+    [self setBorderColor:self.signUpView.passwordField];
+    [self setBorderColor:self.signUpView.emailField];
+    
+    [self.signUpView.usernameField setTextColor:[UIColor blackColor]];
+    [self.signUpView.passwordField setTextColor:[UIColor blackColor]];
+    [self.signUpView.emailField setTextColor:[UIColor blackColor]];
+}
+
+- (void)setBorderColor:(UITextField*)field {
+    field.layer.cornerRadius=5.0f;
+    field.layer.masksToBounds=YES;
+    field.layer.borderColor=[[UIColor darkGrayColor]CGColor];
+    field.layer.borderWidth= .2f;
+    
+    CALayer *layer = field.layer;
+    layer.shadowOpacity = 0.0;
+    layer.backgroundColor = [[UIColor lightTextColor] CGColor];
 }
 
 - (void)viewDidLayoutSubviews {
